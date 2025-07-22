c0fcd2695a989902ca83cdf5acc1ed86039b8eac
m0o0nman <160515856+m0o0nman@users.noreply.github.com>
2025-07-22 19:08:30 +0600

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input: String = String::new();
+
+    println!("Enter your name: ");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read your name.");
+    let name = input.trim();
+
+    let mut input_age: String = String::new();
+    println!("Enter your age: ");
+    io::stdin()
+        .read_line(&mut input_age)
+        .expect("Failed to read your age.");
+    let age: i32 = input_age
+        .trim()
+        .parse()
+        .expect("Please type a number!");
+
+    println!("Your name: {}\nyour age: {}", name, age);
+
 }
