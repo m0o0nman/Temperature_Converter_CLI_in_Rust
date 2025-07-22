23a37ceace13c7219ff609c5372f09c7c98dd16d
m0o0nman <160515856+m0o0nman@users.noreply.github.com>
2025-07-22 19:41:11 +0600

    testing input

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,32 @@
 use std::io;
 //
 fn main() {
-    let mut input: String = String::new();
+    println!("==========  Welcome to Temperature Converter!  ==========");
+    println!("Select features:\n\n1. Celcius to Farenheit\n2. Farenheit to Celcius");
 
-    println!("Enter your name: ");
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read your name.");
-    let name = input.trim();
+    let mut input_choice: String = String::new();
+    let mut choice: i32 = 0;
 
-    let mut input_age: String = String::new();
-    println!("Enter your age: ");
-    io::stdin()
-        .read_line(&mut input_age)
-        .expect("Failed to read your age.");
-    let age: i32 = input_age
-        .trim()
-        .parse()
-        .expect("Please type a number!");
+    let mut flag: bool = true;
 
-    println!("Your name: {}\nyour age: {}", name, age);
+    while flag == true {
+        
+        io::stdin()
+            .read_line(&mut input_choice)
+            .expect("Couldnt Read. Try again!");
+
+        let choice: i32 = input_choice
+            .trim()
+            .parse()
+            .expect("Please enter a valid number");
+
+        if choice == 1 || choice == 2 {flag = false;} else {continue;}  
+        println!("Wrong choice, try again.") 
+    }
+
+    
+
+    print!("Your choice was {}", choice);
+    
 
 }
