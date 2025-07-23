23513797b7ccf4cafa14b430b019ba64fd884c84
m0o0nman <160515856+m0o0nman@users.noreply.github.com>
2025-07-23 17:49:13 +0600

    initally complete, may further optimize later

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,45 @@ fn main() {
         }
     }
 
-    println!("You chose: {}", choice);
+    if choice == 1 {
+        println!("Enter temp in celcius: ");
+        let mut input: String = String::new();
+        let output: f32;
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Please enter a number");
+
+        output = input
+            .trim()
+            .parse()
+            .expect("Couldn't read line.");
+
+        println!("{:.2}C in farenheit: {:.2}F.", output, c_to_f(output));
+    } else {
+        println!("Enter temp in farenheit: ");
+        let mut input: String = String::new();
+        let output: f32;
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Please enter a number");
+
+        output = input
+            .trim()
+            .parse()
+            .expect("Couldn't read line.");
+
+        println!("{:.2}F in celcius: {:.2}C.", output, f_to_c(output));
+    }
     
 
+}
+
+fn c_to_f(celcius: f32) -> f32 {
+    (1.8 * celcius) + 32.0
+}
+
+fn f_to_c (farenheit: f32) -> f32 {
+    (farenheit - 32.0) * 0.56
 }
